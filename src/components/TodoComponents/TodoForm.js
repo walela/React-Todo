a70bdf9954debdef45ba97ff955db633ff01f573
walela <austinmasibo@gmail.com>
2020-02-10 23:07:03 +0300

    Added buttons to TodoForm component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,13 +4,30 @@ import styled from "styled-components"
 const InputField = styled.input`
   width: 30vw;
   padding: 1.2rem;
+  border: 1px solid grey;
+  border-radius: 8px;
+  font-size: 1.5rem;
+  font-family: "Rubik";
+`
+
+const StyledButton = styled.button`
+  font-family: "Rubik";
+  font-size: 1.8rem;
+  margin-left: 12px;
+  border-radius: 2px;
+  box-shadow: 2px 2px 5px grey;
+  margin-right: 12px;
+  padding: 5px;
+  background-color: ${props => props.bgColor || "lightgreen"};
 `
 class TodoForm extends Component {
   render() {
     return (
-      <form>
+      <div>
         <InputField placeholder="Add Todo" />
-      </form>
+        <StyledButton bgColor="lightblue">Add Todo</StyledButton>
+        <StyledButton>Clear Completed</StyledButton>
+      </div>
     )
   }
 }
